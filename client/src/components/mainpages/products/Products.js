0f9492e651c4ae3e689a7a31fbcf39343f75a80c
Anshuman <sharmaaanshumaan@gmail.com>
2022-09-01 15:58:34 +0530

    done

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,17 +1,82 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GlobalState } from "../../../GlobalState";
 import Loading from "../utils/loading/Loading";
 import ProductItem from "../utils/productItem/ProductItem";
+import axios from "axios";
+// import Loading from "../utils/loading/Loading";
 
 const Products = () => {
   const state = useContext(GlobalState);
+  const [check, setCheck] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const [products, setProducts] = state.productAPI.products;
-  const [isAdmin] = state.UserAPI.isAdmin;  
+  const [isAdmin] = state.UserAPI.isAdmin;
   const [token] = state.token;
   const [callBack, setCallback] = state.productAPI.callBack;
+  const checkAll = () => {
+    setCheck(!check);
+    if (!check) {
+      products.forEach((item) => {
+        item.checked = true;
+      });
+    } else {
+      products.forEach((item) => {
+        item.checked = false;
+      });
+    }
+    setProducts([...products]);
+  };
 
+  const deleteAll = async () => {
+    console.log("hello");
+    if (window.confirm(`ARE YOU SURE, YOU WANT TO DELETE THE PRODUCTS?`)) {
+      products.forEach(async (product) => {
+        if (product.checked) {
+          try {
+            setLoading(!loading);
+            const destoryImag = await axios.post(
+              "/api/destroy",
+              {
+                public_id: product.images.public_id,
+              },
+              {
+                headers: { Authorization: token },
+              }
+            );
+
+            const deletedItem = await axios.delete(
+              `/api/products/${product._id}`,
+              {
+                headers: { Authorization: token },
+              }
+            );
+            setLoading(!loading);
+            setCallback(!callBack);
+          } catch (error) {
+            console.log(error.response.data.message);
+          }
+        }
+      });
+    }else{return;}
+  };
   return (
     <>
+      {isAdmin ? (
+        <div className="delete_all">
+          <span>Select All</span>
+          <input
+            type="checkbox"
+            name="checked"
+            id="checked"
+            checked={check}
+            onChange={() => checkAll()}
+          />
+          <button onClick={() => deleteAll()}>Delete All</button>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="products">
         {products.map((products) => {
           return (
--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -1,50 +1,66 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { GlobalState } from "../../../../GlobalState";
-import axios from 'axios';
+import axios from "axios";
 import Loading from "../loading/Loading";
 
-
-const ProductItem = ({ product, isAdmin, token, callBack, setCallback, setProducts}) => {
+const ProductItem = ({
+  product,
+  isAdmin,
+  token,
+  callBack,
+  setCallback,
+  setProducts,
+}) => {
   const state = useContext(GlobalState);
   const addCart = state.UserAPI.addToCart;
   const [loading, setLoading] = useState(false);
-  // const [checkValue, setCheckValue] = useState(false);
+  const [checkValue, setCheckValue] = useState(product.checked);
 
   const deleteProducts = async () => {
     console.log(product);
     try {
-      if(window.confirm(`ARE YOU SURE TO DELETE ${product.title}`)){
+      if (window.confirm(`ARE YOU SURE TO DELETE ${product.title}`)) {
         setLoading(!loading);
-        const destoryImag = await axios.post('/api/destroy', {
-          public_id : product.images.public_id,
-        }, {
-          headers : {Authorization : token}
-        })
-  
+        const destoryImag = await axios.post(
+          "/api/destroy",
+          {
+            public_id: product.images.public_id,
+          },
+          {
+            headers: { Authorization: token },
+          }
+        );
+
         const deletedItem = await axios.delete(`/api/products/${product._id}`, {
-          headers : {Authorization : token}
-        })
-      }else{
+          headers: { Authorization: token },
+        });
+      } else {
         return;
       }
       setLoading(!loading);
       setCallback(!callBack);
-      
     } catch (error) {
-      console.log(error.response.data.message)
+      console.log(error.response.data.message);
     }
-  }
+  };
 
   const handleCheckBox = async (product) => {
-    console.log(product);
-  }
+    setCheckValue(!checkValue);
+    product.checked = !product.checked;
+    console.log(product.checked)
+  };
 
-
-  if(loading) return <Loading/>
+  if (loading) return <Loading />;
   return (
     <div className="product_card">
-      {isAdmin && <input type="checkbox" checked={product.checked} onChange={() => handleCheckBox(product)}/>}
+      {isAdmin && (
+        <input
+          type="checkbox"
+          checked={product.checked}
+          onChange={() => handleCheckBox(product)}
+        />
+      )}
       <img src={product.images.url} alt="" />
 
       <div className="product_box">
