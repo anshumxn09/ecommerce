575a54ecae2c0a484516a4274424526078025b9c
Anshuman <sharmaaanshumaan@gmail.com>
2022-09-19 20:36:35 +0530

    done

--- client/src/components/mainpages/utils/Carousel/Carousell.js
+++ client/src/components/mainpages/utils/Carousel/Carousell.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import './Carousell.css';
+import React, { useState } from 'react'
+import { Slide } from 'react-slideshow-image';
+import { myImages } from './ImageData';
 
 const Carousell = () => {
+  const [autoplay] = useState(true);
   return (
-    <Carousel autoPlay infiniteLoop showArrows={false} className="main-slide">
-        <div>
-            <img src="https://res.cloudinary.com/anshumxn09/image/upload/v1662139716/test/vansh_tnfno6.jpg" alt="Raftaar" />
-        </div>
-        <div>
-            <img src="https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/raftaar_2.jpg" alt="Raftaar" />
-        </div>
-        <div>
-            <img src="https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/raftaar_2.jpg" alt="Raftaar" />
-        </div>
-    </Carousel>
+    <>
+      <section className="hero-images slider">
+        <Slide transitionDuration={500} indicators autoplay={autoplay}>
+          {myImages.map((image) => (
+            <div key={image.id} className="hero slide">
+                <img
+                  id={image.id}
+                  className="responsive"
+                  src={image.src}
+                  alt={image.alt}
+                />
+            </div>
+          ))}
+        </Slide>
+      </section>
+    </>
   )
 }
 
---
+++ client/src/components/mainpages/utils/Carousel/ImageData.js
@@ -0,0 +1,7 @@
+export const myImages = [
+    {
+        id : 1,
+        src : "https://res.cloudinary.com/anshumxn09/image/upload/v1662139716/test/vansh_tnfno6.jpg",
+        alt : "Images",
+    }
+]
